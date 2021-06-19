aee854e2145711504a89e5386887754089381e0a
German Ramirez <mitsuominagi@gmail.com>
2021-06-19 17:22:36 -0500

    Added helpers

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,15 +1,17 @@
 const router = require('express').Router();
-const { Post } = require('../models');
+const { Post,User,Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, async (req, res) => {
   try {
-    const postData = await Post.findAll();
+    const postData = await Post.findAll({
+      include:[{model: User}]
+    });
 
     const posts = postData.map((postInfo) =>
     postInfo.get({ plain: true })
     );
-
+    console.log(posts)
     res.render('homepage', {
       posts,
       logged_in: req.session.logged_in,
@@ -42,5 +44,30 @@ router.get('/signup', (req, res) => {
     loginLink:" active "
   });
 });
+
+
+// GET one post
+router.get('/post/:id', withAuth, async (req, res) => {
+  try {
+    const dbPostData = await Post.findByPk(req.params.id);
+
+    const postInfo = dbPostData.get({ plain: true });
+    console.log(postInfo)
+    res.render('post', { postInfo, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
 
 module.exports = router;
