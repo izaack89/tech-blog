a412f66b802fb122c80f78ea6f657e2c5b34de63
German Ramirez <mitsuominagi@gmail.com>
2021-06-18 19:12:59 -0500

    Edited the relations of the models

--- models/index.js
+++ models/index.js
@@ -1,12 +1,27 @@
 const User = require('./User');
 const Post = require('./Post');
+const Comment = require('./Comment');
 
-User.hasMany(Post, {
-  foreignKey: 'user_id',
-});
 
-Post.belongsTo(User, {
-  foreignKey: 'user_id',
-});
+User.belongsToMany(Post, {
+    // With this code I can define the foreign keys (this is another way to do it https://sequelize.org/master/manual/advanced-many-to-many.html)
+    through: {
+      model: Comment,
+      unique: false
+    },
+    // Define an alias for when data is retrieved
+    as: 'user_comments'
+  });
+  
+  
+  Post.belongsToMany(User, {
+    // With this code I can define the foreign keys (this is another way to do it https://sequelize.org/master/manual/advanced-many-to-many.html)
+    through: {
+      model: Comment,
+      unique: false
+    },
+    // Define an alias for when data is retrieved
+    as: 'post_comments'
+  });
 
-module.exports = { User,Post };
+module.exports = { User,Post,Comment };
