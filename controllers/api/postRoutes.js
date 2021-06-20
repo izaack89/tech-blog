c373105d41be0c6bc475ada936b4ec7f079b0957
German Ramirez <mitsuominagi@gmail.com>
2021-06-19 19:21:10 -0500

    Added the functionality of add post

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -3,17 +3,48 @@ const { Post } = require('../../models');
 
 router.post('/', async (req, res) => {
     try {
-      const userData = await Post.create(req.body);
-  
-      req.session.save(() => {
-        req.session.user_id = userData.id;
-        req.session.logged_in = true;
-  
-        res.status(200).json(userData);
+      const postData = await Post.create({
+        title:req.body.title,
+        content:req.body.content,
+        user_id:req.session.user_id ,
       });
+      res.status(200).json(postData);
     } catch (err) {
       console.log(err)
       res.status(400).json(err);
+    }
+  });
+
+router.put('/:id', async (req, res) => {
+    try {
+        const postData = await Post.update({
+            title:req.body.title,
+            content:req.body.content},{ 
+            where: {
+                id: req.params.id,
+            },
+        });
+
+      res.status(200).json(postData);
+    } catch (err) {
+      console.log(err)
+      res.status(400).json(err);
+    }
+  });
+
+  router.delete('/:id', async(req, res) => {
+  
+    try {
+      const postData = await Post.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
+  
+      res.status(200).json(postData);
+    } catch (err) {
+        console.log(err)
+      res.status(500).json(err);
     }
   });
 module.exports = router;
---
+++ public/assets/js/addPost.js
@@ -0,0 +1,21 @@
+const postFormHandler = async (event) => {
+    event.preventDefault();
+    const title = document.querySelector('#title').value.trim();
+    const content = document.querySelector('#content').value.trim();
+  
+    if (title && content) {
+      const response = await fetch('/api/posts/', {
+        method: 'POST',
+        body: JSON.stringify({ title, content }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location ="/dashboard";
+      } else {
+        alert('Failed to create a Post');
+      }
+    }
+  };
+  
+  document.querySelector('#addPost').addEventListener('submit', postFormHandler);
